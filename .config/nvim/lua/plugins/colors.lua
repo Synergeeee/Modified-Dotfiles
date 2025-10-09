eeead28546588925dacf46a5ba459dd88a78540a
synergeeee <kimapour@gmail.com>
2025-10-08 21:42:30 -0700

    configured the new pywal plugin for nvim

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -10,14 +10,34 @@
 
 
 
-return {
-    "AlphaTechnolog/pywal.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-        -- Set up pywal and load the colors
-        require("pywal").setup()
+--return {
+--    "AlphaTechnolog/pywal.nvim",
+--    lazy = false,
+--    priority = 1000,
+--    config = function()
+       -- Set up pywal and load the colors
+--        require("pywal").setup()
+--
+--    end,
+--}
 
+return {
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000, -- Ensures this colorscheme loads first
+    opts = {
+        plugins = {
+             lazy = true,
+             alpha = true,
+             markdown = false,
+             noice = true,
+             neotree = true,
+             lazygit = true,
+        },
+    },
+    config = function(_,opts)
+    require("neopywal").setup(opts)
+    vim.cmd.colorscheme("neopywal")
     end,
 }
-
--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,30 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	config = function()
-		local config = require("nvim-treesitter.configs")
-		config.setup({
-			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
-		})
-	end,
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+        local config = require("nvim-treesitter.configs")
+        config.setup({
+            auto_install = true,
+            highlight = {
+                enable = true,
+                disable = { "html" },
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            ensure_install = {
+                "markdown",
+                "c",
+                "cpp",
+                "html",
+                "css",
+                "javascript",
+                "typescript",
+                "java",
+                "kotlin",
+                "lua",
+            },
+            sync_install = true,
+        })
+    end,
 }
+
